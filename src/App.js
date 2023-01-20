5c05a84ae39ec6429925da75c28b0c561c783883
Lyuhitto <contact@sujungkim.dev>
2023-01-20 22:52:08 +0900

    Specify the default layout
    Specify the default layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Header from './sections/Header/Header';
+import Navbar from './sections/Navbar/Navbar';
+import SkillAndStack from './sections/SkillAndStack/SkillAndStack';
+import Education from './sections/Education/Education';
+import ProjectList from './sections/ProjectList/ProjectList';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <SkillAndStack />
+      <Education />
+      <ProjectList />
+    </>
+  );
 }
 
 export default App;
