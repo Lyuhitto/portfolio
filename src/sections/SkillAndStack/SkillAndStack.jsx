16153c45b25bd31a07a7e3b5b647d71deb753a36
Lyuhitto <contact@sujungkim.dev>
2023-01-22 14:27:10 +0900

    Make draft of Skill and Stack section
    Make draft of Skill and Stack section.

--- src/sections/SkillAndStack/SkillAndStack.jsx
+++ src/sections/SkillAndStack/SkillAndStack.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 export default function SkillAndStack() {
   return (
-    <div>SkillAndStack</div>
-  )
+    <div>
+      <header>
+        <h2>기술 스택</h2>
+        <span>포트폴리오에 사용한 기술</span>
+      </header>
+
+      <div>
+        <h3>라이브러리</h3>
+        리액트
+      </div>
+      <div>
+        <h3>마크업</h3>
+        HTML, CSS, Post CSS, Tailwind
+      </div>
+      <div>
+        <h3>언어</h3>
+        자바스크립트, 타입스크립트, 파이썬, PHP
+      </div>
+      <div>
+        <h3>개발 도구</h3>
+        Git, Github, Visual Studio Code
+      </div>
+      <div>
+        <h3>자격증</h3>
+        컴퓨터 활용능력 1급 (필기)
+        네트워크 관리사 2급
+        토익 R/C 805점
+        JLPT 일본어 능력 시험 1급
+        ITパスポート(일본)
+      </div>
+    </div>
+  );
 }
