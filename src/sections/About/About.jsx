9ef3c6ece087bb45beff5a2ff06fc8a912d705fe
Lyuhitto <contact@sujungkim.dev>
2023-01-22 14:02:04 +0900

    Implement contact section
    Implement contact me section of About.

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+import { BsGithub } from 'react-icons/bs';
+import { SiBlogger } from 'react-icons/si';
+import { HiOutlineMail } from 'react-icons/hi';
+import { MdUpdate } from 'react-icons/md';
 
 export default function About() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <div>
+        <h2>도전하고 기록하는</h2>
+        <h1>
+          김수정<span> 입니다.</span>
+        </h1>
+        <p></p>
+      </div>
+      <div>
+        <a href='/' target='_blank'>
+          <BsGithub />
+          깃허브
+        </a>
+        <a href='/' target='_blank'>
+          <SiBlogger />
+          블로그
+        </a>
+        <span>
+          <HiOutlineMail />
+          이메일
+        </span>
+        <span>
+          <MdUpdate />
+          최근 갱신일: 2023/01/22
+        </span>
+      </div>
+    </div>
+  );
 }
