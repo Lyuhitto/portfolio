644b564b89b07ab8c4c08a838b899ec9b37255ff
Lyuhitto <contact@sujungkim.dev>
2023-01-23 21:53:47 +0900

    Implement stack list with map
    Implement stack list with map method. It will add description button(after).

--- src/sections/SkillAndStack/SkillAndStack.jsx
+++ src/sections/SkillAndStack/SkillAndStack.jsx
@@ -7,31 +7,54 @@ export default function SkillAndStack() {
         <h2>기술 스택</h2>
         <span>포트폴리오에 사용한 기술</span>
       </header>
-
-      <div>
-        <h3>라이브러리</h3>
-        리액트
-      </div>
-      <div>
-        <h3>마크업</h3>
-        HTML, CSS, Post CSS, Tailwind
-      </div>
-      <div>
-        <h3>언어</h3>
-        자바스크립트, 타입스크립트, 파이썬, PHP
-      </div>
-      <div>
-        <h3>개발 도구</h3>
-        Git, Github, Visual Studio Code
-      </div>
-      <div>
-        <h3>자격증</h3>
-        컴퓨터 활용능력 1급 (필기)
-        네트워크 관리사 2급
-        토익 R/C 805점
-        JLPT 일본어 능력 시험 1급
-        ITパスポート(일본)
-      </div>
+      {stackList.map((stack, key) => (
+        <div key={key}>
+          <h3>{stack.title}</h3>
+          <ul>
+            {stack.item.map((i, k) => (
+              <li key={k}>
+                <div>
+                  <img src={i.img} alt='' />
+                  <span>{i.name}</span>
+                  {i.desc && <button>설명</button>}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
+const stackList = [
+  { title: '라이브러리', item: [{ name: '리액트' }] },
+  {
+    title: '마크업',
+    item: [
+      { name: 'HTML', img: 'img' },
+      { name: 'CSS', img: 'img' },
+      { name: 'Post CSS' },
+      { name: 'TailWind' },
+    ],
+  },
+  {
+    title: '언어',
+    item: [
+      { name: '자바스크립트' },
+      { name: '타입스크립트' },
+      { name: '파이썬' },
+      { name: 'PHP' },
+    ],
+  },
+  { title: '개발 도구', item: [{ name: 'Git' }, { name: 'Github' }] },
+  {
+    title: '자격증',
+    item: [
+      { name: '컴퓨터 활용능력 1급 (필기)' },
+      { name: '네트워크 관리사 2급' },
+      { name: '토익 R/C 805점' },
+      { name: 'JLPT 일본어 능력 시험 1급' },
+      { name: 'ITパスポート(일본)', desc:'ITパスポート, IT 패스포트란, ' },
+    ],
+  },
+];
