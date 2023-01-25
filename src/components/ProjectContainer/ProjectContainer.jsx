1f014bdb01e7009c74ce2323c6a1c12b8be2e10b
Lyuhitto <contact@sujungkim.dev>
2023-01-25 21:39:23 +0900

    Create Project list item container
    Create Project list item container draft.

---
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ProjectContainer() {
+  return (
+    <div>
+      이미지
+      개인/팀
+      프로젝트제목
+      참여기간
+      사용한 기술스택
+      특징
+      링크
+    </div>
+  )
+}
